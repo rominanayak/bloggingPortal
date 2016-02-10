8a13979b3688efe4149639648a82d2b6ba4a1dab
rominanayak <rominanayak30@gmail.com>
2016-02-10 00:31:27 -0500

    Adding model classes for comments and updating models & controllers for blogs

--- bloggingPortal/src/bloggingPortal/Migrations/20160210050957_AddComment.Designer.cs
+++ bloggingPortal/src/bloggingPortal/Migrations/20160210050957_AddComment.Designer.cs
@@ -8,9 +8,10 @@ using bloggingPortal.Models;
 namespace bloggingPortal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160210050957_AddComment")]
+    partial class AddComment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
--- bloggingPortal/src/bloggingPortal/Models/Blog.cs
+++ bloggingPortal/src/bloggingPortal/Models/Blog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace bloggingPortal.Models
@@ -6,9 +7,23 @@ namespace bloggingPortal.Models
     public class Blog
     {
         [Key]
+        [ScaffoldColumn(false)]
         public Guid Id { get; set; }
         public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
+        [ScaffoldColumn(false)]
         public DateTime PostedOn { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public string Id { get; set; }
+        public string BlogId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string UserName { get; set; }
+        public string Body { get; set; }
     }
 }
